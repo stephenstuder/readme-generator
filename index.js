a0398bf69d60eeb7140a9b1b311e688d8c2c011a
stephen studer <stephenkstuder@gmail.com>
2020-04-27 10:33:58 -0700

    prompt of questions, axios call to github for data, rough structure

--- index.js
+++ index.js
@@ -1,29 +1,72 @@
-let inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
 
 inquirer
   .prompt([
     {
       type: "input",
-      message: "What is your user name?",
+      message: "Title of the project?",
+      name: "title"
+    },
+    {
+      type: "input",
+      message: "Description of the project",
+      name: "description"
+    },
+    {
+      type: "input",
+      message: "Installation",
+      name: "installation"
+    },
+    {
+      type: "input",
+      message: "License?",
+      name: "license"
+    },
+    {
+      type: "input",
+      message: "Usage",
+      name: "usage"
+    },
+    {
+      type: "input",
+      message: "Contributing",
+      name: "contributing"
+    },
+    {
+      type: "input",
+      message: "Tests:",
+      name: "tests"
+    },
+    {
+      type: "input",
+      message: "Questions:",
+      name: "questions"
+    },
+    {
+      type: "input",
+      message: "Please enter your github username:",
       name: "username"
     },
-    {
-      type: "password",
-      message: "What is your password?",
-      name: "password"
-    },
-    {
-      type: "password",
-      message: "Re-enter password to confirm:",
-      name: "confirm"
-    }
   ])
   .then(function(response) {
+      //Use github name to gather image and email
+        let readMeData = response;
+        const queryUrl = `https://api.github.com/users/${response.username}`;
+        axios
+        .get(queryUrl)
+        .then((response) => {
+          // handle success
+          const avatar_url = response.data.avatar_url;
+          const email = response.data.email;
+        })
+        .catch((error) => {
+          // handle error
+          console.log(error);
+        });
+        
+        //actually write to a file here that we create on the fly. 
+        //fs.writeFile("README.md", )
 
-    if (response.confirm === response.password) {
-      console.log("Success!");
-    }
-    else {
-      console.log("You forgot your password already?!");
-    }
-  });
+      });
